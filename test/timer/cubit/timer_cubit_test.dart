91bdd2c64ff945f5b50dc66bb13a5984c30904a5
4xMafole <4cruxt.unknown@gmail.com>
2023-02-11 13:15:04 +0300

    feat: added cubit method test

--- test/timer/cubit/timer_cubit_test.dart
+++ test/timer/cubit/timer_cubit_test.dart
@@ -1,1 +1,24 @@
-void main() {}
+import 'package:bloc_test/bloc_test.dart';
+import 'package:custom_timer/custom_timer.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:pomodoro/timer/timer.dart';
+
+void main() {
+  group('timerBreak()', () {
+    const cubit = TimerCubit.new;
+
+    group('Pomodoro time to a short break', () {
+      blocTest<TimerCubit, TimerState>(
+        'emits [TimerState] when timerBreak() is added.',
+        build: cubit,
+        act: (bloc) => bloc.timerBreak(),
+        expect: () => const <TimerState>[
+          TimerState(
+            timerBreak: TimerBreak.short,
+            time: 5,
+          )
+        ],
+      );
+    });
+  });
+}
